82bbb679c47bae89fb164f044763308733e88a82
razaghimahdi <razaghimahdi78@gmail.com>
2022-12-10 12:35:57 +0330

    - update dependencies

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -1,9 +1,9 @@
 object Android {
      const val appId= "com.example.movielistbycompose01"
-    const val compileSdk = 31
+    const val compileSdk = 32
     const val buildTools = "30.0.3"
     const val minSdk = 21
-    const val targetSdk = 31
+    const val targetSdk = 32
     const val versionCode = 1
     const val versionName = "1.0"
 }
--- buildSrc/src/main/java/AndroidX.kt
+++ buildSrc/src/main/java/AndroidX.kt
@@ -1,5 +1,5 @@
 object AndroidX {
-    private const val coreKtxVersion = "1.6.0"
+    private const val coreKtxVersion = "1.7.0"
     const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
 
     private const val appCompatVersion = "1.3.0"
