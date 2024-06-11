2f866b0d0f0b00de61fc345960b73f0a035f54b8
razzaghi.m <razzaghi.m@tiddev.com>
2024-06-11 10:18:21 +0330

    - Updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "androidsupportmultidexversion.txt"
         }
     }
 }
